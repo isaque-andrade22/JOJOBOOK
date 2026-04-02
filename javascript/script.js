2c5ac897090fc2a6c66cc1946498015770173271
Lucas 'lutaw <lucasbarbosa81612@gmail.comm>
2026-04-02 04:01:13 -0300

    Refatoração: Apagando os 9 CSS e tirando todo o Despair do projeto >_<! Puhuhuhu 🐻

---
+++ javascript/script.js
@@ -0,0 +1,2 @@
+// Renomeie para n dar conflito com qualquer outra pagina, pq se tivermos uma "jojo.html" no futuro vai dar ruim.
+// TOMA CUIDADO COM OS NOMES GURI, PARA DE CHAMAR OS TRENS TUDO DE JOJO 
